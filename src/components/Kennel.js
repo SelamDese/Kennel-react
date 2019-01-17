4fdcdd1eaef36b5d60bf334bead5748ff6cca37a
selam <selam0315@gmail.com>
2019-01-17 15:36:19 -0600

    state and props learning

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,15 +1,50 @@
 import React, { Component } from 'react'
 import EmployeeList from "./employee/EmployeeList"
 import LocationList from "./location/LocationList"
+import AnimalList from "./animals/AnimalList"
+import "./Kennel.css"
 
-export default class Kennel extends Component {
+class Kennel extends Component {
+
+    /*
+        Although you will eventually be pulling your objects
+        from your json-server API, for this chapter, we're
+        faking it and just creating those arrays in the component
+        itself
+    */
+    employeesFromAPI = [
+        { id: 1, name: "Jessica Younker" },
+        { id: 2, name: "Jordan Nelson" },
+        { id: 3, name: "Zoe LeBlanc" },
+        { id: 4, name: "Blaise Roberts" }
+    ]
+
+    // This will eventually get pulled from the API
+    locationsFromAPI = [
+        { id: 1, name: "Nashville North", address: "500 Circle Way" },
+        { id: 2, name: "Nashville South", address: "10101 Binary Court" }
+    ]
+
+    animalsFromAPI = [
+        { id: 1, name: "Dog", },
+        { id: 2, name: "Cat", }
+    ]
+
+    state = {
+        employees: this.employeesFromAPI,
+        locations: this.locationsFromAPI,
+        animals: this.animalsFromAPI
+    }
+
     render() {
         return (
-            <div>
-                <h3>Student Kennels</h3>
-                <LocationList />
-                <EmployeeList />
-            </div>
-        );
+            <article className="kennel">
+                <LocationList locations={this.state.locations} />
+                <EmployeeList employees={this.state.employees} />
+                <AnimalList animals={this.state.animals} />
+            </article>
+        )
     }
 }
+
+export default Kennel
--- src/components/location/LocationList.js
+++ src/components/location/LocationList.js
@@ -4,13 +4,16 @@ import React, { Component } from 'react'
 export default class LocationList  extends Component {
     render() {
         return (
-            <article>
-                <h1>Location List</h1>
-                <h4>Nashville North Location</h4>
-                <h5>500 Puppy Way</h5>
-                <h4>Nashville South Location</h4>
-                <h5>200 Puppy Way</h5>
-            </article>
+            <section className="employees">
+            {
+                this.props.locations.map(location =>
+                    <div key={location.id}>
+                      <p>{location.name}</p>  
+                        <p>{location.address}</p>
+                    </div>
+                )
+            }
+            </section>
         );
     }
 }
